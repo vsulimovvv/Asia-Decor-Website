8f9c044f04828f2105bbb5137878a9a1a51f88fe
vsulimovvv <super.sulimov2014@yandex.ru>
2021-11-16 17:26:53 +0300

    начало проекта

---
+++ app/js/main.js
@@ -0,0 +1,85 @@
+window.addEventListener('DOMContentLoaded', () => {
+  // var swiper = new Swiper(".productions__slider", {
+  //   slidesPerView: 4,
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     type: "progressbar",
+  //   },
+  //   navigation: {
+  //     nextEl: ".swiper-button-next",
+  //     prevEl: ".swiper-button-prev",
+  //   },
+  // });
+
+
+  // const mixer = mixitup('.productions__wrapper');
+
+  const sliderGroup = () => {
+    const sliderRec = document.querySelector('.productions__slider');
+    const nextBtn = document.querySelector('.arrow-next');
+    const prevBtn = document.querySelector('.arrow-prev');
+
+    if (sliderRec) {
+      const swiper = new Swiper(sliderRec, {
+        spaceBetween: 29,
+        navigation: {
+          nextEl: nextBtn,
+          prevEl: prevBtn,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 2.1,
+            spaceBetween: 8,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+          },
+          1200: {
+            slidesPerView: 4,
+            spaceBetween: 29,
+          },
+        },
+      });
+    }
+  };
+  sliderGroup();
+});
+
+// 
+// (function toggleSelect() {
+//   $('#select').wSelect();
+// })()
+
+
+// const showMenu = () => {
+//   const menuBtn = document.querySelector('.toggle-menu');
+//   const menu = document.querySelector('.header__bottom');
+//   const body = document.querySelector('body');
+//   // const overlay = document.querySelector('.overlay');
+//   const closeBtn = document.querySelector('.header__bottom-close');
+
+//   menuBtn.addEventListener('click', e => {
+//     menu.classList.toggle('active');
+//     // menuBtn.classList.toggle('active');
+//     // overlay.classList.toggle('active');
+//     body.classList.toggle('no-scroll');
+//   });
+
+//   // overlay.addEventListener('click', e => {
+//   // menu.classList.remove('active');
+//   // menuBtn.classList.remove('active');
+//   // overlay.classList.remove('active');
+//   // body.classList.remove('no-scroll');
+//   // });
+//   closeBtn.addEventListener('click', e => {
+//     menu.classList.remove('active');
+//     menuBtn.classList.remove('active');
+//     // overlay.classList.remove('active');
+//     body.classList.remove('no-scroll');
+//   });
+// };
+// showMenu();
